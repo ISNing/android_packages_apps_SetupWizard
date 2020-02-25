ee3b597ff2e740b2336ec1acb22327c65bbaa2ed
SF2005 <1208733578@qq.com>
2020-02-25 22:06:55 +0800

    SetupWizard:change package name
    Change-Id: I01b4eab8ad9fc123a66b9a588b11416e6af4b8b4

--- start_lineage_wizard_with_gms.sh
+++ start_lineage_wizard_with_gms.sh
@@ -6,7 +6,7 @@ adb shell pm enable com.google.android.setupwizard || true
 wait ${!}
 adb shell pm disable com.android.provision || true
 wait ${!}
-adb shell am start org.lineageos.setupwizard/org.lineageos.setupwizard.SetupWizardTestActivity
+adb shell am start org.exthmui.setupwizard/org.exthmui.setupwizard.SetupWizardTestActivity
 wait ${!}
 sleep 1
 adb shell am start com.google.android.setupwizard/com.google.android.setupwizard.SetupWizardTestActivity
